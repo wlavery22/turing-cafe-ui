641859db12635dab4f000f6ace53ff42d2d1d517
William Lavery <lavery_william@yahoo.com>
2024-01-08 12:36:53 -0500

    Add 1st tests

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,23 @@
-describe('empty spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
+describe("Visit the homepage", () => {
+  beforeEach(() => {
+    cy.intercept("GET", "http://localhost:3001/api/v1/reservations", {
+      statusCode: 200,
+      fixture: "reservations",
+    }).as("homepage");
+    cy.visit("http://localhost:3000");
+  });
+  it("should display homepage with reservations", () => {
+    cy.intercept("POST", "http://localhost:3001/sightings", {
+      statusCode: 201,
+      fixture: "post",
+    }).as("postSightings");
 })
+
+
+
+
+// describe('empty spec', () => {
+//   it('passes', () => {
+//     cy.visit('https://example.cypress.io')
+//   })
+// })
